d42049a5289a21810f89e82c1abfa17ba8d91dfa
root <root@ip-10-133-74-219.ap-south-1.compute.internal>
2026-02-26 08:53:26 +0000

    Added cost optimization Lambda integration

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import ChatBot from "./components/ChatBot";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+	  return (
+		      <div>
+		        <Dashboard />
+		        <ChatBot />
+		      </div>
+		    );
+}
+
+export default App;
