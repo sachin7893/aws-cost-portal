15174255c9e53b48036aeda0bf95a36a8f7c96ba
sachin7893 <54388116+sachin7893@users.noreply.github.com>
2026-02-26 22:25:25 +0530

    Update FinOpsPage.js

--- src/pages/FinOpsPage.js
+++ src/pages/FinOpsPage.js
@@ -1,15 +1,32 @@
+import React, { useEffect, useState } from "react";
+
 function FinOpsPage() {
-	  return (
-		      <>
-		        <h2>FinOps Optimization</h2>
-		        <p>
-		          - Cost anomalies detection
-		          - Savings Plans & RI recommendations
-		          - Idle resource detection
-		          - Cost breakdown analysis
-		        </p>
-		      </>
-		    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://0azsdk6qbb.execute-api.ap-south-1.amazonaws.com/prod/finops")
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>FinOps Monthly Cost</h2>
+
+      {data.length === 0 ? (
+        <p>No data found...</p>
+      ) : (
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              {item.month} — ${item.total_cost.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default FinOpsPage;
