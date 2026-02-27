7bea9a06635ad73b35fd05f949aeb31961693c34
root <root@ip-10-131-22-36.ap-south-1.compute.internal>
2026-02-27 05:55:59 +0000

    changed index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material";
 import App from "./App";
-import theme from "./theme";
+import { ThemeProvider } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
