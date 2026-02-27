af5df1d0d56f61857ec43e1230a5b0c227ca043b
sachin7893 <54388116+sachin7893@users.noreply.github.com>
2026-02-27 21:44:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import FinOpsPage from "./pages/FinOpsPage";
 import CloudOpsPage from "./pages/CloudOpsPage";
 import EC2Page from "./pages/EC2Page";
 import ECSPage from "./pages/ECSPage";
-import "./global.css";
+import EKSPage from "./pages/EKSPage";
 
-function App({ toggleTheme, mode }) {
-	  return (
-		      <Router>
-		        <div className="page-container">
-		          <Sidebar />
-
-		          <div className="main-wrapper">
-		            <div className="topbar">
-		              <div>🚀 FinOps CloudOps Portal</div>
-
-		              <button className="theme-btn" onClick={toggleTheme}>
-		                {mode === "light" ? "🌙 Dark" : "☀ Light"}
-		              </button>
-		            </div>
-
-		            <Routes>
-		              <Route path="/" element={<Dashboard />} />
-		              <Route path="/finops" element={<FinOpsPage />} />
-		              <Route path="/cloudops" element={<CloudOpsPage />} />
-		              <Route path="/ec2" element={<EC2Page />} />
-		              <Route path="/ecs" element={<ECSPage />} />
-		            </Routes>
-		          </div>
-		        </div>
-		      </Router>
-		    );
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/finops" element={<FinOpsPage />} />
+      <Route path="/cloudops" element={<CloudOpsPage />} />
+      <Route path="/ec2" element={<EC2Page />} />
+      <Route path="/ecs" element={<ECSPage />} />
+      <Route path="/eks" element={<EKSPage />} />
+    </Routes>
+  );
 }
 
 export default App;
