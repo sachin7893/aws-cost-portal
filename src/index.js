cf1196b13dbcf291f31819a9c12e79f83cdcb596
sachin7893 <54388116+sachin7893@users.noreply.github.com>
2026-02-27 11:39:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@mui/material";
 import App from "./App";
-import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-	  <ThemeProvider theme={theme}>
-	    <App />
-	  </ThemeProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
